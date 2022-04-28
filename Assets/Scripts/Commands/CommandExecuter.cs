655f02b75de682cb39391aecffc9aef0d154e525
Laszlo Kocsis <kocsislaci95@gmail.com>
2022-04-28 17:27:34 +0200

    feat(unitcommandmanagement): can send multiple commands to unit and they execute them fifo

--- Assets/Scripts/Commands/CommandExecuter.cs
+++ Assets/Scripts/Commands/CommandExecuter.cs
@@ -1,14 +1,71 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CommandExecuter : MonoBehaviour
 {
     private List<ICommand> _commands = new List<ICommand>();
-
     public List<ICommand> Commands
     {
-        get => _commands;
-        set => _commands = value;
+        private get => _commands;
+        set
+        {
+            _commands = value;
+        }
     }
+
+    private bool isExecuting = false;
+    private NavMeshAgent _navMeshAgent;
+
+    private void Awake()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        StartCoroutine(Idle());
+    }
+    // tuskes banankigyo 
+    public void AddCommand(ICommand command)
+    {
+        _commands.Add(command);
+    }
+    public void ResetCommands()
+    {
+        _commands.Clear();
+        isExecuting = false;
+    }
+    
+    IEnumerator Idle()
+    {
+        while (true) // infinite loop
+        {
+            if (_commands.Count > 0 && !isExecuting)
+            {
+                ICommand command = _commands[0];
+                if (command.GetType().Equals(typeof(MoveCommand)))
+                {
+                    StartCoroutine(Move(((MoveCommand) command)._targetPos));
+                }
+            }
+            yield return new WaitForSeconds(.1f);
+        }
+        
+    }
+    IEnumerator Move(Vector3 targetPos)
+    {
+        _navMeshAgent.SetDestination(targetPos);
+        isExecuting = true;
+        while (isExecuting)
+        {
+            yield return new WaitForSeconds(.1f);
+            if ((targetPos - this.transform.position).magnitude < 0.2f)
+            {
+                _commands.RemoveAt(0);
+                isExecuting = false;
+            }
+        }
+    }
+
+    
+    
 }
--- Assets/Scripts/Commands/ICommand.cs
+++ Assets/Scripts/Commands/ICommand.cs
@@ -1,29 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
 
 public interface ICommand
 {
-    public void Execute();
+    // public void Execute(GameObject executor);
 }
 public class MoveCommand : ICommand
 {
-    public void Execute()
+    public Vector3 _targetPos;
+    public MoveCommand(Vector3 clickPos)
     {
-        throw new System.NotImplementedException();
+        _targetPos = clickPos;
     }
+
 }
-public class AttackCommand : ICommand
-{
-    public void Execute()
-    {
-        throw new System.NotImplementedException();
-    }
-}
-public class GatherCommand : ICommand
-{
-    public void Execute()
-    {
-        throw new System.NotImplementedException();
-    }
-}
+// public class AttackCommand : ICommand
+// {
+//     public void Execute()
+//     {
+//         throw new System.NotImplementedException();
+//     }
+// }
+// public class GatherCommand : ICommand
+// {
+//     public void Execute()
+//     {
+//         throw new System.NotImplementedException();
+//     }
+// }
